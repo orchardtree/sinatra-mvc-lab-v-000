a13fa31c5d83c93ea30396aacd6c172b603c131c
Jesse Wenger <wengerj@gmail.com>
2021-04-06 19:07:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,13 +5,8 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  get '/piglatinize' do
-    erb :piglatinize
-  end
-  
   post '/piglatinize' do
     @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :piglatinize
-    redirect '/piglatinize'
   end
 end
