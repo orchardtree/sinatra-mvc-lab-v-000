0d728cc45fb4dd9333c2a50fc7fe59e3fe68094c
Jesse Wenger <wengerj@gmail.com>
2021-04-06 20:04:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,8 +17,8 @@ class PigLatinizer
     case word
       when /\A[aeiouAEIOU]/
         new_word = letters_after(0) + "way"
-      when /\A[aeiouAEIOU]/
-        new_word = letters_after(0) + "way"
+      when /\A[w][h]
+        new_word = letters_after(1) + first_letters(1) + "ay"
       when /\A[p](l|r)/
         new_word = letters_after(2) + first_letters(2) + "ay"
       when /\A[t][h]/
