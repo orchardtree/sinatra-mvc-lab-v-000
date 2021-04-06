edc57941068affa4ce9a3a974d7818975284b821
Jesse Wenger <wengerj@gmail.com>
2021-04-06 17:23:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  
+  post '/' do
+    @piglatinized_text = Piglatinizer.new(params[:user_text])
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class PigLatinizer
+  
+  def piglatinize(phrase)
+    @phrase_ary = phrase.split(" ")
+    @piglatin_ary = @phrase_ary.collect do |word|
+      handle_piglatin(word)
+    end
+    @piglatin_ary.join(" ")
+  end
+  
+  def handle_piglatin(word)
+    @word = word
+    case word
+      when /\A[aeiouAEIOU]/
+        new_word = letters_after(0) + "way"
+      when /\A[p](l|r)/
+        new_word = letters_after(2) + first_letters(2) + "ay"
+      when /\A[t][h]/
+        new_word = letters_after(2) + first_letters(2) + "ay"
+      when /\A[s][p][r]/
+        new_word = letters_after(3) + first_letters(3) + "ay"
+      else
+        new_word = letters_after(1) + first_letters(1) + "ay"
+    end    
+  end
+  
+  def first_letters(i)
+    i -= 1
+    @word[0..i]
+  end
+  
+  def letters_after(i)
+    ii = @word.length
+    @word[i..ii]
+  end
+end
